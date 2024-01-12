03c707f8ca38747e4db7c57b7c17abef2de12fdf
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2024-01-13 01:31:38 +0500

    Add project files.

---
+++ WpfApp20/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp20
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click0(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickIshora(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickNuqta(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickTeng(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click6(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickMinus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickPlus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickKupay(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickBuluv(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickIldiz(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickKvadrat(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickBirBuluv(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickBittaBittaUchirish(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickTozalash(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickFoiz(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click9(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click8(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click7(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
